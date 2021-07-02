94e0c73a5a4adc06d16c27c061abade84c870fec
Hannah Weinstein <hannah.weinstein@pennmedicine.upenn.edu>
2021-07-02 12:32:43 -0400

    hello world

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_143129) do
+ActiveRecord::Schema.define(version: 2021_07_02_143718) do
+
+  create_table "people", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
