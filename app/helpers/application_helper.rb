c930ff19080fc7ba6c3a8ebfb48e825e51b22d86
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2022-01-14 17:16:54 -0500

    Add Hotwire, set up basic working implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     base << 'active' if request.path == path
     link_to(text, path, class: base.join(' '))
   end
+
+  def render_alerts
+    turbo_stream.update 'alerts', partial: 'shared/flash'
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Request < ApplicationRecord
+  after_create_commit { broadcast_prepend_to 'requests' }
+  after_update_commit { broadcast_replace_to 'requests' }
+  after_destroy_commit { broadcast_remove_to 'requests' }
+
   belongs_to :point_person, class_name: 'Techie'
   belongs_to :requester, class_name: 'Person'
 
